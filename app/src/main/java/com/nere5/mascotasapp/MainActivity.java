f025db689a208ce8351237db9f2c36936aaaf5ce
Nere <nere5@hotmail.com>
2020-10-15 21:58:29 -0500

    Fragments vacios

--- app/src/main/java/com/nere5/mascotasapp/MainActivity.java
+++ app/src/main/java/com/nere5/mascotasapp/MainActivity.java
@@ -2,8 +2,10 @@ package com.nere5.mascotasapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,29 +13,53 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.google.android.material.tabs.TabLayout;
+
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     ArrayList<Mascota> listaMascotas;
     RecyclerView recyclerMascotas;
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar miActionBar = (Toolbar) findViewById(R.id.miActionBar);
-        setSupportActionBar(miActionBar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        setUpViewPager();
 
-        listaMascotas = new ArrayList<Mascota>();
+
+       /* listaMascotas = new ArrayList<Mascota>();
         recyclerMascotas = (RecyclerView) findViewById(R.id.rcvMascotas);
         recyclerMascotas.setLayoutManager(new LinearLayoutManager(this));
-        llenarMascotas();
+        llenarMascotas();*/
 
+        /*  AdaptadorMascotas adapter = new AdaptadorMascotas(listaMascotas);
+        recyclerMascotas.setAdapter(adapter);*/
 
-        AdaptadorMascotas adapter = new AdaptadorMascotas(listaMascotas);
-        recyclerMascotas.setAdapter(adapter);
+        if (toolbar != null){
+            setSupportActionBar(toolbar);
+        }
+
+    }
+
+    private ArrayList<Fragment> agregarFragments(){
+        ArrayList<Fragment> fragments = new ArrayList<>();
+        fragments.add(new RecyclerViewFragment());
+        fragments.add(new PerfilFragment());
+        return fragments;
+    }
+
+    private void setUpViewPager(){
+        viewPager.setAdapter(new PageAdapter(getSupportFragmentManager(), agregarFragments()));
+        tabLayout.setupWithViewPager(viewPager);
     }
 
     private void llenarMascotas() {
@@ -48,6 +74,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.action_view, menu);
+        getMenuInflater().inflate(R.menu.menu_opciones, menu);
         return true;
     }
 
@@ -56,6 +83,14 @@ public class MainActivity extends AppCompatActivity {
             case R.id.atvFavoritos:
                 Intent intent = new Intent(this, MascotasFavoritas.class);
                 startActivity(intent);
+                break;
+            case R.id.Contacto:
+                Intent i = new Intent(this, FormularioContacto.class);
+                startActivity(i);
+                break;
+            case R.id.AcercaDe:
+                Intent e = new Intent(this, Biografia.class);
+                startActivity(e);
                 break;
         }
         return super.onOptionsItemSelected(item);
